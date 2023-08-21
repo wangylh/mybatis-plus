09b61d70360aed270de5f974fd0c09e0202e65cc
wangylh <wangylh@126.com>
2023-08-21 16:48:35 +0800

    mybatisplus普通查询

--- src/test/java/com/li/mybatisplus/MybatisPlusApplicationTests.java
+++ src/test/java/com/li/mybatisplus/MybatisPlusApplicationTests.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class MybatisPlusApplicationTests {
@@ -24,6 +27,23 @@ class MybatisPlusApplicationTests {
         List<User> users = userMapper.selectList(null);
         Assert.notEmpty(users, String.valueOf(users.size()));
         users.forEach(System.out::println);
+    }
+
+    @Test
+    void selectTest1(){
+        User user = userMapper.selectById(1693535919040638978L);
+        System.out.println(user);
+
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1693535919040638978L, 1693542516752244737L));
+        users.forEach(System.out::println);
+
+        Map<String, Object> map = new HashMap<>(){
+            {
+                put("name", "习近平");
+            }
+        };
+        List<User> users1 = userMapper.selectByMap(map);
+        System.out.println(users1);
     }
 
 }
