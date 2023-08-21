c462f1fd7284274b543b5c870248395ec110a9e1
wangylh <wangylh@126.com>
2023-08-21 21:07:23 +0800

    mybatisplus条件查询

--- src/test/java/com/li/mybatisplus/MybatisPlusApplicationTests.java
+++ src/test/java/com/li/mybatisplus/MybatisPlusApplicationTests.java
@@ -1,6 +1,7 @@
 package com.li.mybatisplus;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.li.mybatisplus.dao.UserMapper;
 import com.li.mybatisplus.entity.User;
@@ -25,21 +26,21 @@ class MybatisPlusApplicationTests {
     }
 
     @Test
-    void selectTest(){
+    void selectTest() {
         List<User> users = userMapper.selectList(null);
         Assert.notEmpty(users, String.valueOf(users.size()));
         users.forEach(System.out::println);
     }
 
     @Test
-    void selectTest1(){
+    void selectTest1() {
         User user = userMapper.selectById(1693535919040638978L);
         System.out.println(user);
 
         List<User> users = userMapper.selectBatchIds(Arrays.asList(1693535919040638978L, 1693542516752244737L));
         users.forEach(System.out::println);
 
-        Map<String, Object> map = new HashMap<>(){
+        Map<String, Object> map = new HashMap<>() {
             {
                 put("name", "习近平");
             }
@@ -49,7 +50,7 @@ class MybatisPlusApplicationTests {
     }
 
     @Test
-    void selectWrapperTest(){
+    void selectWrapperTest() {
         //QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         //QueryWrapper<User> queryWrapper = Wrappers.<User>query();
         QueryWrapper<User> queryWrapper = Wrappers.query();
@@ -66,6 +67,65 @@ class MybatisPlusApplicationTests {
         queryWrapper.likeRight("name", "王").or().ge("age", 25).orderByDesc("age").orderByAsc("id");
         List<User> users2 = userMapper.selectList(queryWrapper);
         users2.forEach(System.out::println);
+    }
+
+    @Test
+    void selectWrapper1Test() {
+        QueryWrapper<User> query = Wrappers.query();
+        query.apply("date_format(create_time,'%Y-%m-%d')={0}", "2019-01-14").inSql("manager_id", "select id from user where name like '王%'");
+        List<User> users = userMapper.selectList(query);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    void selectWrapper2Test() {
+        QueryWrapper<User> query = Wrappers.query();
+        query.likeRight("name", "王").and(wq -> wq.lt("age", 40).or().isNotNull("email"));
+        List<User> users = userMapper.selectList(query);
+        users.forEach(System.out::println);
+
+        query.clear();
+        query.likeRight("name", "王").or(wq -> wq.lt("age", 40).gt("age", 20).isNotNull("email"));
+        List<User> users1 = userMapper.selectList(query);
+        users1.forEach(System.out::println);
+
+        query.clear();
+        query.nested(wq -> wq.lt("age", 40).or().isNotNull("email")).likeRight("name", "王");
+        List<User> users2 = userMapper.selectList(query);
+        users2.forEach(System.out::println);
+
+        query.clear();
+        query.in("age", Arrays.asList(31,32,33,34));
+        List<User> users3 = userMapper.selectList(query);
+        users3.forEach(System.out::println);
+
+        query.clear();
+        query.in("age", Arrays.asList(31,32,33,34)).last("limit 1");
+        List<User> users4 = userMapper.selectList(query);
+        users4.forEach(System.out::println);
+    }
+
+    @Test
+    void selectWrapper3Test() {
+        QueryWrapper<User> queryWrapper = Wrappers.query();
+        queryWrapper.select("id","name").like("name", "雨").lt("age", 40);
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+
+        queryWrapper.clear();
+        queryWrapper.select(User.class, info -> !info.getColumn().equals("create_time") && !info.getColumn().equals("email")).like("name", "雨").lt("age", 40);
+        List<User> users1 = userMapper.selectList(queryWrapper);
+        users1.forEach(System.out::println);
+    }
+
+    @Test
+    void selectWrapper4Test() {
+        String name = "";
+        String email = "l";
+        QueryWrapper<User> queryWrapper = Wrappers.query();
+       queryWrapper.like(StringUtils.isNotEmpty(name), "name", name).like(StringUtils.isNotEmpty("email"), "email", email);
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
     }
 
 }
