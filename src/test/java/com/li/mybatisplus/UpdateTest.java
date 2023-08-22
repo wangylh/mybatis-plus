211382f7f799f590783fc9183cec02ebf1c29fdc
wangylh <wangylh@126.com>
2023-08-22 10:25:40 +0800

    更新

--- src/test/java/com/li/mybatisplus/UpdateTest.java
+++ src/test/java/com/li/mybatisplus/UpdateTest.java
@@ -1,12 +1,14 @@
 package com.li.mybatisplus;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.conditions.update.LambdaUpdateChainWrapper;
 import com.li.mybatisplus.dao.UserMapper;
 import com.li.mybatisplus.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDateTime;
 
 /**
  * Author: lh
@@ -21,14 +23,46 @@ public class UpdateTest {
     private UserMapper userMapper;
 
     @Test
-    void insertTest() {
-        User user = User.builder().name("习近平").age(60).managerId(1087982257332887553L).createTime(LocalDateTime.now()).build();
-        userMapper.insert(user);
+    void updateTest() {
+        int i = userMapper.updateById(User.builder().name("赵乐际").id(1693542516752244737L).build());
+        System.out.println("影响记录数：" + i);
     }
 
     @Test
-    void insertTest1() {
-        User user = User.builder().name("李强").age(60).managerId(1087982257332887553L).createTime(LocalDateTime.now()).remark("备注").build();
-        userMapper.insert(user);
+    void updateTest1() {
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("name", "赵乐际");
+        int update = userMapper.update(User.builder().name("王沪宁").build(), updateWrapper);
+        System.out.println("影响记录数" + update);
+    }
+
+    @Test
+    void updateTest2() {
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>(User.builder().name("王沪宁").build());
+        int update = userMapper.update(User.builder().name("蔡奇").build(), updateWrapper);
+        System.out.println("影响记录数" + update);
+    }
+
+    @Test
+    void updateTest3() {
+        UpdateWrapper<User> updateWrapper = Wrappers.update();
+        updateWrapper.set("name", "丁薛祥").eq("name", "蔡奇");
+        int update = userMapper.update(null, updateWrapper);
+        System.out.println("影响记录数" + update);
+    }
+
+    @Test
+    void updateTest4() {
+        LambdaUpdateWrapper<User> lambdaUpdateWrapper = Wrappers.lambdaUpdate();
+        lambdaUpdateWrapper.set(User::getName, "韩正").eq(User::getName, "丁薛祥");
+
+        int update = userMapper.update(null, lambdaUpdateWrapper);
+        System.out.println("影响记录数" + update);
+    }
+
+    @Test
+    void updateTest5() {
+        boolean update = new LambdaUpdateChainWrapper<>(userMapper).set(User::getName, "胡锦涛").eq(User::getName, "韩正").update();
+        System.out.println("影响记录数" + update);
     }
 }
